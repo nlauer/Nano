f81a8aef6e35d24128db460602724e082e754748
Nick Lauer <nlauer9@gmail.com>
2014-09-06 19:57:55 -0700

    Created more urls for FB and yelp and texting

---
+++ Nano/NanoTodayExtension/Shortcut.h
@@ -0,0 +1,16 @@
+//
+//  Shortcut.h
+//  Nano
+//
+//  Created by Nick Lauer on 2014-09-06.
+//  Copyright (c) 2014 Lauer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Shortcut : NSObject
+
+@property (strong, nonatomic) NSString *url;
+@property (strong, nonatomic) NSString *name;
+
+@end
