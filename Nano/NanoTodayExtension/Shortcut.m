ecab2b7eb0acb75e52d6ec335ecb3637ac0bb698
Nick Lauer <nlauer9@gmail.com>
2014-09-07 05:05:11 -0700

    Moved custom shortcuts to be class methods on the shortcut model

--- Nano/NanoTodayExtension/Shortcut.m
+++ Nano/NanoTodayExtension/Shortcut.m
@@ -10,6 +10,112 @@
 
 @implementation Shortcut
 
++ (Shortcut *)venmoShortcutWithRecipient:(NSString *)recipient amount:(NSUInteger)amount message:(NSString *)message
+{
+    Shortcut *shortcut = [[Shortcut alloc] init];
+    shortcut.recipient = recipient;
+    shortcut.amount = amount;
+    shortcut.message = message;
+    shortcut.icon = @"venmo";
+
+    return shortcut;
+}
+
++ (Shortcut *)googleMapsShortcutFrom:(NSString *)from to:(NSString *)to mode:(NSString *)mode
+{
+    Shortcut *shortcut = [[Shortcut alloc] init];
+    shortcut.name = [NSString stringWithFormat:@"%@ to %@", mode, to];
+    shortcut.icon = @"google";
+    shortcut.url = [self googleMapsURLFrom:from to:to mode:mode];
+
+    return shortcut;
+}
+
++ (NSURL *)googleMapsURLFrom:(NSString *)from to:(NSString *)to mode:(NSString *)mode
+{
+    return [NSURL URLWithString:[NSString stringWithFormat:@"comgooglemaps://?saddr=%@&daddr=%@&directionsmode=%@", from, to, mode]];
+}
+
++ (Shortcut *)uberShortcutFrom:(NSString *)from to:(NSString *)to
+{
+    Shortcut *shortcut = [[Shortcut alloc] init];
+    shortcut.name = [NSString stringWithFormat:@"Uber to %@", to];
+    shortcut.icon = @"uber";
+    shortcut.url = [self uberURLForFrom:from to:to];
+
+    return shortcut;
+}
+
++ (NSURL *)uberURLForFrom:(NSString *)from to:(NSString *)to
+{
+    return [NSURL URLWithString:[NSString stringWithFormat:@"uber://?action=setPickup&pickup[formatted_address]=%@&[formatted_address]=%@", from, to]];
+}
+
++ (Shortcut *)smsShortcutForNumber:(NSString *)number name:(NSString *)name
+{
+    Shortcut *shortcut = [[Shortcut alloc] init];
+    shortcut.name = [NSString stringWithFormat:@"Text Message %@", name];
+    shortcut.icon = @"imessage";
+    shortcut.url = [self smsURLForPhoneNumber:number];
+
+    return shortcut;
+}
+
++ (NSURL *)smsURLForPhoneNumber:(NSString *)number
+{
+    return [NSURL URLWithString:[NSString stringWithFormat:@"sms:%@", number]];
+}
+
++ (Shortcut *)yelpShortcutForSearch:(NSString *)search
+{
+    Shortcut *shortcut = [[Shortcut alloc] init];
+    shortcut.name = [NSString stringWithFormat:@"Search Yelp for %@", search];
+    shortcut.icon = @"yelp";
+    shortcut.url = [self yelpSearchForName:search];
+
+    return shortcut;
+}
+
++ (NSURL *)yelpSearchForName:(NSString *)name
+{
+    return [NSURL URLWithString:[NSString stringWithFormat:@"yelp:///search?terms=%@", name]];
+}
+
++ (Shortcut *)yoShortcutForRecipient:(NSString *)recipient
+{
+    Shortcut *shortcut = [[Shortcut alloc] init];
+    shortcut.name = [NSString stringWithFormat:@"YO %@", recipient];
+    shortcut.recipient = recipient;
+    shortcut.icon = @"yo";
+
+    return shortcut;
+}
+
++ (Shortcut *)facebookEventShortcutForEventID:(NSString *)eventID eventName:(NSString *)eventName
+{
+    Shortcut *shortcut = [[Shortcut alloc] init];
+    shortcut.name = eventName;
+    shortcut.icon = @"fb";
+    shortcut.url = [self facebookEventURLForEventID:eventID];
+
+    return shortcut;
+}
+
++ (NSURL *)facebookEventURLForEventID:(NSString *)eventID
+{
+    return [NSURL URLWithString:[NSString stringWithFormat:@"fb://event?id=%@", eventID]];
+}
+
++ (Shortcut *)rdioShortcutForWebURLString:(NSString *)urlString
+{
+    Shortcut *shortcut = [[Shortcut alloc] init];
+    shortcut.name = @"Listen to Coding Mix";
+    shortcut.icon = @"rdio";
+    shortcut.url = [NSURL URLWithString:[urlString stringByReplacingOccurrencesOfString:@"http" withString:@"rdio"]];
+
+    return shortcut;
+}
+
 - (void)encodeWithCoder:(NSCoder *)encoder {
     //Encode properties, other class variables, etc
     [encoder encodeObject:self.url forKey:@"url"];
